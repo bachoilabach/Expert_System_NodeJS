2baba412e4888a5891907dbb679f04655f48594b
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-11-07 16:03:39 +0700

    using cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+import cors from 'cors';
+
 const app = express();
 
 // init middlewares
+app.use(cors())
 app.use(express.json());
 app.use(
 	express.urlencoded({
